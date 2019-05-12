4e46c9347d2f6266947df310e14a02aaccbf0619
Gabriel Pereira da Silva <gabriel.gps@outlook.com>
2019-05-12 19:32:42 -0300

    Listing items with FlatList component.

--- App.js
+++ App.js
@@ -7,61 +7,40 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, Image, View, Dimensions} from 'react-native';
-// import logo from './resources/img/s2.png';
+import {Platform, StyleSheet, Text, Image, View, Dimensions, FlatList} from 'react-native';
 
 const width = Dimensions.get('screen').width;
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    const users = [
+    const photos = [
       {
         id: 1,
-        name: 'Gabriel'
+        user: 'Gabriel'
       },
       {
         id: 2,
-        name: 'John Doe'
+        user: 'John Doe'
       }
     ];
 
     return (
-      <View>
-        <Text>Hello</Text>
-        {users.map(user =>
-          <View key={user.id}>
-            <Text>{user.name}</Text>
-            <Image source={require('./resources/img/s2-checked.png')} style={{width, height: width}} />
-          </View>
-        )}
+      <View style={{ marginTop: 20 }}>
+        <FlatList
+          data={photos}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) =>
+            <View>
+              <Text>{item.user}</Text>
+              <Image
+                source={require('./resources/img/s2-checked.png')}
+                style={{width, height: width}}
+              />
+            </View>
+          }
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
